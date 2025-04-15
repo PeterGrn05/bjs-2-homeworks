b8862a322b8ff648c9fbe6a48e679e7f7b175286
Горин Пётр <peter14102005@gmail.com>
2025-04-15 13:21:35 +0300

    Поправил код, а то почему то жалуется

--- 1.base-concepts/task.js
+++ 1.base-concepts/task.js
@@ -1,22 +1,26 @@
 "use strict"
 function solveEquation(a, b, c) {
   let arr = [];
-  let discriminant = b ** 2 - 4 * a * c;
-  if (discriminant > 0) {
-    arr[0] = (-b + Math.sqrt(discriminant)) / (2 * a);
-    arr[1] = (-b - Math.sqrt(discriminant)) / (2 * a);
-  } else if (discriminant == 0) {
-    arr[0] = -b / (2 * a);
+  const discriminant = Math.pow(b, 2) - 4 * a * c;
+  if (discriminant === 0) {
+    const root = -b / (2 * a);
+    arr.push(root);
+  } else if (discriminant > 0) {
+    const root1 = (-b + Math.sqrt(discriminant)) / (2 * a);
+    const root2 = (-b - Math.sqrt(discriminant)) / (2 * a);
+    arr.push(root1, root2);
   }
   return arr;
 }
-solveEquation(1, 5, 4)
+
 
 function calculateTotalMortgage(percent, contribution, amount, countMonths) {
-  let monthPercent = percent / 100 / 12;
-  let change = amount - contribution;
-  let monthPay = change * (monthPercent + monthPercent / ((1 + monthPercent) ** countMonths - 1));
-  let AllPayments = Number((monthPay * countMonths).toFixed(2));
-  return AllPayments;
+  let monthlyRate = percent / 100 / 12;
+  let loanAmount = amount - contribution;
+  if (loanAmount <= 0) {
+    return 0;
+  }
+  let monthlyPayment = loanAmount * (monthlyRate + (monthlyRate / (Math.pow(1 + monthlyRate, countMonths) - 1)));
+  let totalAmount = monthlyPayment * countMonths;
+  return parseFloat(totalAmount.toFixed(2));
 }
-calculateTotalMortgage(10, 0, 10000, 36)
